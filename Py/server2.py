1120e5c3d08c8ba661771bfe0804f5cc487f92a1
Nadir Syed <nadir.syedsammut@gmail.com>
2023-02-11 16:03:58 +0100

    still broken

--- Py/server2.py
+++ Py/server2.py
@@ -22,13 +22,16 @@ class server:
         self.message = bytearray()
         self.part = bytearray()
         self.myData = Game.Observation()
-        self.completedMessage = None
+        self.fullData = False
+        self.connection = None
+        self.clientAddress =  None
+        self.excpt = False
 
     def connect(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serverAddress =(self.ip, self.port)
+        # self.sock.setblocking(False)
         self.sock.bind(serverAddress)
-        self.sock.setblocking(False)
         self.sock.listen(1)
         print('starting up on {} port {}'.format(*serverAddress))
     
@@ -42,11 +45,11 @@ class server:
             data.extend(packet)
         return data
     
-    def toNump(self, im):
+    def toNumpy(self, im):
         im.load()
         # unpack data
-        e = Image._getencoder(im.mode, 'raw', im.mode)
-        e.setimage(im.im)
+        tmpImage = Image._getencoder(im.mode, 'raw', im.mode)
+        tmpImage.setimage(im.im)
 
         # NumPy buffer for the result
         shape, typestr = Image._conv_type_shape(im)
@@ -61,57 +64,60 @@ class server:
             raise RuntimeError("encoder error %d in tobytes" % s)
         return data
     
-    def decode_img(self):
-        size = (self.completedMessage.screenData.width, self.completedMessage.screenData.height)
-        if self.completedMessage.screenData.bpp == 0:
+    def decodeImg(self):
+        size = (self.myData.SS.width, self.myData.SS.height)
+        if self.myData.SS.bpp == 0:
             # not actually 16bpp... BGR555
-            self.pic = self.toNumpy(Image.frombuffer("RGB", size, self.completedMessage.screenData.data, 'raw', 'BGR;15', 0, 1))
-        elif self.completedMessage.screenData.bpp == 1:
-            self.pic = self.toNumpy(Image.frombuffer("RGB", size, self.completedMessage.screenData.data, 'raw', 'BGR', 0, 1))
+            self.pic = self.toNumpy(Image.frombuffer("RGB", size, self.myData.SS.data, 'raw', 'BGR;15', 0, 1))
+        elif self.myData.screenData.bpp == 1:
+            self.pic = self.toNumpy(Image.frombuffer("RGB", size, self.myData.SS.data, 'raw', 'BGR', 0, 1))
         else:
             self.pic = None
         
     def receive(self):
-        while True:
-            try:
-                self.connection, self.clientAddress = self.sock.accept()
-                print('connection from', self.clientAddress)
-                print((self.mState == messageState.mPing.name))
-                if self.mState == messageState.mPing.name:
-                    self.buffer = self.recvall(1) # can be removed later
-                    self.mState = messageState.mRecvHeader.name
+        #print((self.mState == messageState.mPing.name))
+        try:
+            if self.mState == messageState.mPing.name:
+                self.buffer = self.recvall(1) # can be removed later
+                self.mState = messageState.mRecvHeader.name     
 
-                if self.mState == messageState.mRecvHeader.name:
-                    self.part = bytearray()
-                    if len(self.header) < 4:
-                        self.part = self.connection.recv(4 - len(self.header))
-                        self.header.extend(self.part)
-                    else:
-                        self.mSize = int.from_bytes(self.header, 'little')
-                        self.mState = messageState.mRecvData.name
-                        
-                if self.mState == messageState.mRecvData.name:
-                    self.part = bytearray()
-                    if len(self.message) < self.mSize:
-                        self.part = self.connection(self.mSize - len(self.message))
-                        self.message.extend(self.part)
-                    else:
-                        self.mState = messageState.mPing.name
-                        self.completedMessage = self.myData.ParseFromString(self.message)
-                        self.message = bytearray()
-                        self.header = bytearray()
-                        self.mSize = bytearray()
-                        
-            except socket.error as e:
-                print('GRRRRRRRRRR', self.mState, e)
-                if self.completedMessage is not None:
-                    self.pic = self.decodeImg()
-                    cv2.imshow('window', self.pic)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        cv2.destroyAllWindows()
+            if self.mState == messageState.mRecvHeader.name:
+                self.part = bytearray()
+                if len(self.header) < 4:
+                    self.part = self.connection.recv(4 - len(self.header))
+                    self.header.extend(self.part)
+                else:
+                    self.mSize = int.from_bytes(self.header, 'little')
+                    self.mState = messageState.mRecvData.name
                     
+            if self.mState == messageState.mRecvData.name:
+                self.part = bytearray()
+                if len(self.message) < self.mSize:
+                    self.part = self.connection.recv(self.mSize - len(self.message))
+                    self.message.extend(self.part)
+                else:
+                    self.fullData = True
+                    self.mState = messageState.mPing.name
+                    self.myData.ParseFromString(self.message)
+        except socket.error as e:
+            self.excpt = True            
+
 
 serverSession = server()
 serverSession.connect()
+print("wait for connection")
 while True:
-    serverSession.receive()
+    serverSession.connection, serverSession.clientAddress = serverSession.sock.accept()
+    serverSession.connection.setblocking(False)
+    print('connection from', serverSession.clientAddress)
+    while True:
+        serverSession.receive()
+        print(serverSession.mState)
+        #print('GRRRRRRRRRR', self.mState, e)
+        if serverSession.excpt and serverSession.fullData is not None:
+            serverSession.excpt = False
+            serverSession.pic = serverSession.decodeImg()
+            cv2.imshow('window', serverSession.pic)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                
