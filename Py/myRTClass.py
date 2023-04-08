626160553b5f105741e335f457d868863133ffeb
Nadir Syed <nadir.syedsammut@gmail.com>
2023-04-08 22:21:27 +0200

    tiny rtgym progress

--- Py/myRTClass.py
+++ Py/myRTClass.py
@@ -3,21 +3,37 @@ from rtgym import RealTimeGymInterface
 from serverClass import server 
 import gymnasium.spaces as spaces
 import numpy as np
+import logging
 
 class MyGranTurismoRTGYM(RealTimeGymInterface):
-    def __init__(self,debugFlag=False,histSize=4):
+    def __init__(self, debugFlag=False, displayHist=3):
         print("GT Real Time instantiated")
         self.server = server(debug=debugFlag)
         self.display = None
         self.gamepad = None
-        self.histSize = histSize
+        self.histSize = displayHist
 
     def startSession(self):
         self.server.connect()
+        logging.debug("Server session started")
         
     def init_control(self):
         import vgamepad as vg
         self.gamepad = vg.VDS4Gamepad()
+        logging.debug("Virtual Dual Shock 4 loaded")
+        
+    def inititalizeCommon(self):
+        self.init_control()
+        self.startSession()
+        
+    def getDataImage(self):
+        self.server.receiveOneFrame
+        # Maybe better not to copy but....
+        # myVS = self.server.myData.VS
+        # myGS = self.server.myData.GS
+        # posVect = self.server.myData.posVect       
+        return (self.server.myData.VS, self.server.myData.GS, self.server.myData.posVect, self.server.pic)
+    
 
     # Mandatory method        
     def get_observation_space(self):
@@ -31,8 +47,9 @@ class MyGranTurismoRTGYM(RealTimeGymInterface):
         # fRighttSlip = spaces.Box(low=0, high=256, shape=(1,))
         # rLeftSlip = spaces.Box(low=0, high=256, shape=(1,))
         # rRightSlip = spaces.Box(low=0, high=256, shape=(1,))
-        view = spaces.Box(low=0.0, high=255.0, shape=(self.histSize, 240, 320, 3))        
-        return spaces.Tuple((vSpeed,vSteer,view))
+        display = spaces.Box(low=0.0, high=255.0, shape=(self.histSize, 240, 320, 3))
+        position = spaces.Box(low=-3000000, high=3000000, shape=(2,))        
+        return spaces.Tuple((position, vSpeed, vSteer, display))
     
     # Mandatory method
     def get_action_space(self):
@@ -54,6 +71,11 @@ class MyGranTurismoRTGYM(RealTimeGymInterface):
     def send_control(self, control):
         controlGamepad(self.gamepad, control)
 
+    # Optional method
     def wait(self):
         self.send_control(self.get_default_action())
         # self.server.reloadSave()
+        
+    # Optional method
+    def render(self):
+        pass
