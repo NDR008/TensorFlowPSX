44ff1419eca1c06423f8846f6387bbf4cc08f41a
Nadir Syed <nadir.syedsammut@gmail.com>
2023-11-26 14:04:51 +0100

    fix

--- Py/rlGT_multiV4_4.py
+++ Py/rlGT_multiV4_4.py
@@ -22,7 +22,7 @@ max_training_steps_per_env_step = 1.0
 start_training = 0 #2e5 # waits for... 1000
 device = trainer_device
 MODEL_MODE = 2
-CONTROL_MODE = 1.5
+CONTROL_MODE = 2
 CARCHOICE = 0
 
 if CARCHOICE == 1:
