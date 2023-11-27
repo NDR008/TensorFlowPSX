943856a744e9df304d27f26fcdec661203e1787c
Nadir Syed <nadir.syedsammut@gmail.com>
2023-11-27 19:30:15 +0100

    tweak

--- Py/rlGT_multiV4_5.py
+++ Py/rlGT_multiV4_5.py
@@ -24,7 +24,7 @@ update_model_interval = 500  # 2000 #steps 1000
 max_training_steps_per_env_step = 1.0
 start_training = 0 #2e5 # waits for... 1000
 device = trainer_device
-MODEL_MODE = 3
+MODEL_MODE = 2
 CONTROL_MODE = 2
 CARCHOICE = 0
 
